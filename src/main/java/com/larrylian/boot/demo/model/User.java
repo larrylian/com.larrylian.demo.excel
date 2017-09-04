4739070fd1766f5dab9b4f50d2c099f70b194a66
Larry <554538252@qq.com>
2017-09-04 10:36:54 +0800

    '修改所有时间字段都为Date类型

--- src/main/java/com/larrylian/boot/demo/model/User.java
+++ src/main/java/com/larrylian/boot/demo/model/User.java
@@ -17,11 +17,11 @@ public class User {
     private String email;
     private Date birthday;
     //创建时间
-    private Timestamp created_at;
+    private Date created_at;
     //最新更新时间
-    private Timestamp updated_at;
+    private Date updated_at;
     //删除时间
-    private Timestamp deleted_at;
+    private Date deleted_at;
 
     public int getId() {
         return id;
@@ -79,27 +79,27 @@ public class User {
         this.birthday = birthday;
     }
 
-    public Timestamp getCreated_at() {
+    public Date getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Timestamp created_at) {
+    public void setCreated_at(Date created_at) {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdated_at() {
+    public Date getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(Timestamp updated_at) {
+    public void setUpdated_at(Date updated_at) {
         this.updated_at = updated_at;
     }
 
-    public Timestamp getDeleted_at() {
+    public Date getDeleted_at() {
         return deleted_at;
     }
 
-    public void setDeleted_at(Timestamp deleted_at) {
+    public void setDeleted_at(Date deleted_at) {
         this.deleted_at = deleted_at;
     }
 }
